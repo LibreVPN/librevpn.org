27a2865ad673e298eb731ec9935472d1f226300f
Nicolás Reynolds <fauno@endefensadelsl.org>
2014-04-15 17:19:07 -0300

    hardcodear nombres de host

--- bin/crear_nodos_3.py
+++ bin/crear_nodos_3.py
@@ -13,9 +13,29 @@ import networkx as nx
 from networkx.readwrite import json_graph
 from random import choice
 
+hosts = [ 'nexus', 'pvnoidentificado', 'marcopolo', 'parabola',
+'BlackWood', 'tardis', 'gauss', 'brendanvps', 'robotica', 'clementina',
+'almendro', 'susa', 'hilarious', 'lancelot', 'PuntoRojo1', 'i2x',
+'karlitoxz', 'rasputin', 'bender', 'scarlet', 'rapanui', 'dvm',
+'edoras', 'atlantis', 'pauloat', 'uchi', 'ealing', 'carioca', 'orion',
+'britta', 'andromeda', 'smeagol', 'maxpower', 'dzup', 'threepwood',
+'c3p0', 'jara', 'hexodica', 'ExodicaWorld', 'saboteur', 'ponape',
+'dojonet', 'cooperativa_libertad', 'nbtrimwork', 'x0r', 'feston',
+'medieval', 'perceval', 'barney2', 'kagada', 'amaretto', 'sh4r3m4n',
+'hat', 'diablo', 'kelvin', 'haiti', 'retro', 'atom', 'anastasia',
+'noanoa', 'libertatia', 'apolo', 'conan', 'victoria', 'jarvis', 'ciego',
+'morgana', 'WTF', 'jack', 'arpon', 'fibonacci', 'berreta', 'kaus',
+'d0h', 'galleguindio', 'zorak', 'caipiroska', 'tesla', 'rigel', 'pixie',
+'relogi' ]
+
 nodes = int(argv[1])
 min_links = int(argv[2])
 max_links = int(argv[3])
+
+# TODO expandir hosts a cantidad de nodos
+if len(hosts) <= nodes:
+  print "La cantidad de nodos es mayor a la cantidad de hosts actual"
+  exit()
 
 G= nx.Graph()
 G.add_nodes_from(range(1, nodes))
@@ -24,15 +44,12 @@ G.add_nodes_from(range(1, nodes))
 for n in G.nodes():
 # elegimos al azar la cantidad de enlaces que va a tener
   e = choice(range(min_links,max_links))
-
-  print "%d con %d enlaces" % (n, e)
+  G.node[n]['name'] = hosts[n]
 
 # mientras la cantidad de enlaces sea menor a lo planeado
   while e > len(G.neighbors(n)):
 # elegir otro nodo al azar
     i = choice(range(1,nodes))
-
-    print "%d enlaza a %d" % (n, i)
 
 # y agregar el enlace
     G.add_edge(n, i)
